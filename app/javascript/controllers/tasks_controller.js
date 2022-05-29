6c167284a5cd60cfd6efd336d3ba3d8cb29e7f6e
Shailendra Shukla <shailendrashuklapro@gmail.com>
2022-05-28 19:56:51 -0500

    feat: handling actions in JS

--- app/javascript/controllers/tasks_controller.js
+++ app/javascript/controllers/tasks_controller.js
@@ -5,4 +5,11 @@ export default class TasksController extends Controller {
   connect() {
     console.log('🚀 ~ file: tasks_controller.js ~ line 7 ~ element', this.element);
   }
+
+  toggleCompleted(event) {
+    const id = event.target.dataset.id;
+    console.log('🚀 ~ file: tasks_controller.js ~ line 11 ~ id', id);
+    const completed = event.target.checked;
+    console.log('🚀 ~ file: tasks_controller.js ~ line 12 ~ completed', completed);
+  }
 }
